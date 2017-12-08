4e73005e38698804e071b115f9b6c6c03ba54fb7
rahul4times <tkdmaster.sharma@gmail.com>
2017-12-08 15:38:00 -0700

    commit

--- db/migrations/20171208130336_01cars.js
+++ db/migrations/20171208130336_01cars.js
@@ -6,11 +6,13 @@ exports.up = function(knex, Promise) {
     table.string('model');
     table.string('trim');
     table.string('engine');
+    table.string('drive_type');
     table.string('body_type');
     table.string('ext_color');
     table.string('int_color');
     table.string('transmission');
     table.integer('price');
+    table.integer('sale');
     table.string('vin');
     table.timestamps(true, true);
   });
